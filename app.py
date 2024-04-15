3eb8f5ab7743c18b2b743fc2a352c203dcff7def
estherlau0411 <144003264+estherlau0411@users.noreply.github.com>
2024-04-15 10:15:46 +0000

    update file

--- app.py
+++ app.py
@@ -1,27 +1,4 @@
-from flask import Flask, render_template
+from app import app
 
-app = Flask(__name__)
-
-
-@app.route("/")
-def index():
-    return render_template("index.html")
-
-@app.route("/release")
-def release():
-    return render_template("release.html")
-
-@app.route("/pre_sale")
-def pre_sale():
-    return render_template("pre-sale.html")
-
-@app.route("/payment")
-def payment():
-    return render_template("payment.html")
-
-@app.route("/contate")
-def contate():
-    return render_template("contate.html")
-    
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run()
