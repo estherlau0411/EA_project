2c68d7b2e75512cc7022d03e1b43a9ef26a8850c
estherlau0411 <144003264+estherlau0411@users.noreply.github.com>
2024-04-20 14:46:43 +0000

    update

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
-from app import app
+from flask import Flask, render_template
 
-if __name__ == '__main__':
-    app.run()
+app = Flask(__name__)
+
+
+@app.route("/")
+@app.route("/index")
+def index():
+   return render_template("index.html")
