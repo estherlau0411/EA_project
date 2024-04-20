0937ec75d498d210daa97004ab02f668d27ec65d
estherlau0411 <144003264+estherlau0411@users.noreply.github.com>
2024-04-20 20:24:31 +0800

    Delete app directory

--- app/routes.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-from flask import render_template, flash, redirect, url_for, request, g
-
-@app.route('/')
-def index():
-    title = "百老匯電影"
-    return render_template('index.html.j2')
